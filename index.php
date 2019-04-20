8706fbb3bf59e3e981555096fde40b7ac30e6356
Rafsan15 <rafsanjany26@gmail.com>
2019-04-20 18:06:44 +0600

    New

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ctbd
+ * Date: 4/20/2019
+ * Time: 3:50 PM
+ */
+
+
+?>
+
+
+<html>
+    <head></head>
+    <body>
+        <form action="" method="POST" >
+            <input type="text" name="Name" placeholder="Enter your Name">
+            <input type="submit" value="Submit" name="Submit">
+        </form>
+
+    <?php
+        if(isset($_POST["Submit"])){
+            if(strlen($_POST["Name"])<5 || strlen($_POST["Name"])>10){
+                echo  "<P style='color: red'> Invalid Input</P>";
+            }
+        }
+    ?>
+    </body>
+</html>
